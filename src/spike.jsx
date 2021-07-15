02272b68afad22185bcf48c4532e5d4a4bc4dae6
margaret <marz2000000@gmail.com>
2021-07-14 20:34:15 -0400

    this is not the actual jsx file

--- src/spike.jsx
+++ src/spike.jsx
@@ -1,8 +1,45 @@
-import React from "react"
+import React, { Children } from "react"
 import ReactDom from "react-dom"
 
+function Canvas({children,size}) {
+    
+    return (<svg width = {size} height = {size}> {children}</svg>);
+}
+
+function Polygon({n, radius}) {
+    var pointsA = ""
+    var angle = 2/n*Math.PI
+    var i = 0
+        do {
+            var nextPointX = 100+radius*Math.cos(i*angle)
+            var nextPointY= 100-radius*Math.sin(i*angle)
+            pointsA += nextPointX + "," + nextPointY + " "
+            //console.log("Point X" + (i+1) + ": " + nextPointX)
+            // console.log("Point Y" + (i+1) + ": " +nextPointY)
+            i++
+        }
+        while (i<n) 
+    
+        return (<polygon id="star" 
+            points = "pointsA"
+            fill = "pink"> </polygon>)
+}
+
+
+
 function App() {
-    return (<p>Hello, world!</p>);
+    return (
+       <> <div>   
+            <Canvas size = "500">
+                <Polygon n= {6,50}/>      
+            </Canvas>
+        </div>
+        <div>
+            <Canvas size = "500">
+                <Polygon n= {4,50}/>      
+            </Canvas>
+        </div> </>
+    )
 }
 
 ReactDom.render(<App />, document.getElementById('container'))
