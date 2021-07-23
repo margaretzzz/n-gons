268274db84be8dbd56469c123791f62a569639a8
margaret <marz2000000@gmail.com>
2021-07-22 20:06:05 -0400

    gives coordinates thru developer tools console (does not output an image)

--- src/spike.jsx
+++ src/spike.jsx
@@ -5,48 +5,76 @@ function Canvas({children,size}) {
     return (<svg width = {size} height = {size}> {children}</svg>);
 }
 
-function Polygon({n, radius}) {
-    var pointsA = ""
-    var angle = 2/n*Math.PI
-    var i = 0
-        do {
-            var nextPointX = 100+radius*Math.cos(i*angle)
-            var nextPointY= 100-radius*Math.sin(i*angle)
-            pointsA += nextPointX + "," + nextPointY + " "
-            i++
-        }
-        while (i<n) 
-    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16)
+function Polygon({x,y,n,radius}) {
+    var pointsA = "";
+    var angle = 2/n*Math.PI;
+    var i = 0;
+    while (i < n) {
+            var nextPointX = x+radius*Math.cos(i*angle);
+            var nextPointY= y-radius*Math.sin(i*angle);
+            pointsA +=  nextPointY + "," + nextPointX+ " ";   
+            i=i+1;          
+    }        
+    var pointx = x+radius*Math.cos(0);
+    var pointy = y+radius*Math.sin(0);
+    pointsA +=  pointy + "," + pointx ;  
     return (<polygon id="star" 
-        points = {pointsA}
-        fill = {randomColor}> </polygon>)
+        points = {pointsA} >
+         </polygon>)
 }
 
+
+function polygon({x,y,n,radius}) {
+    var pointsA = [];
+    var angle = 2/n*Math.PI;
+    var i = 0;
+    while (i < n) {
+            var nextPointX = x+radius*Math.cos(i*angle);
+            var nextPointY= y-radius*Math.sin(i*angle);
+            pointsA.push([nextPointY, nextPointX]) ;   
+            i=i+1;          
+    }        
+    var pointx = x+radius*Math.cos(0);
+    var pointy = y+radius*Math.sin(0);
+    pointsA.push([pointy, pointx]) ; 
+    console.log(JSON.stringify({
+        type: "Feature", 
+        geometry: { 
+            type: "Polygon",
+            coordinates:[pointsA]
+        },
+        properties: {}
+    }))
+}
+polygon({x: 40.80962, y: -73.96079, n: 5,radius: .0001})
+function Pointstojson() {
+    var pointsA = Polygon() ;
+    var geo = {
+        "type": "Feature",
+        "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+            pointsA   
+        ]
+        },
+        "properties": {}
+    };
+    console.log(JSON.stringify(geo= {"coordinates": pointsA}))
+}
+//Pointstojson()
+
 function App() {
-    var [spikes,setSpikes] = useState(0)
-    useEffect(()=>{  
-        setInterval (() => {
-            setSpikes(value => value + 1)    
-        }, 1000)
-    }, [])
-    var lst =[1,2,3, 4,5, 6]
-    
     return (
-        <div style = {{position: "relative", width: "1024px", height: "768px"}}> 
-        {
-            lst.map((l)=> {
-                return (
-                <div style = {{position: "absolute", top: 120*l, left: 120*l}}>   
-                <Canvas size = "200">
-                    <Polygon n= {spikes} 
-                        radius = {10*l}/>      
-                </Canvas>
-                </div>
-                )
-            })            
-        }   
-        </div> 
+        <> <div>   
+            <Canvas size = "500">    
+                <Polygon x = {40.80962}
+                        y= {-73.96079} 
+                        n= {6} 
+                        radius = {0.0001}/>      
+            </Canvas>
+            </div> </>    
     )
 }
+
 
 ReactDom.render(<App />, document.getElementById('container'))
